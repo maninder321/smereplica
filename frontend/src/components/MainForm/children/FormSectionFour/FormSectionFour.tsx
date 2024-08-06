c52c219c2f356921d5254a96e704960e46d6a6df
maninder321 <maninderjitsingh380@gmail.com>
2024-08-07 00:19:51 +0530

    --added form section four

--- frontend/src/components/MainForm/children/FormSectionFour/FormSectionFour.tsx
+++ frontend/src/components/MainForm/children/FormSectionFour/FormSectionFour.tsx
@@ -1,7 +1,59 @@
 import React from "react";
+import styles from "./formSectionFour.module.css";
 
 function FormSectionFour() {
-  return <div>FormSectionFour</div>;
+  return (
+    <div className={styles.container}>
+      <div>
+        <input type="checkbox" />
+        By ticking, you are confirming that you have understood and are agreeing
+        to the details mentioned:
+      </div>
+      <div className={styles.information}>
+        <div className={styles.singleInformation}>
+          <span>
+            <i className="fa-solid fa-check"></i>
+          </span>
+          <div>
+            I confirm that I am the authorized person to upload bank statements
+            on behalf of my company
+          </div>
+        </div>
+      </div>
+      <div className={styles.information}>
+        <div className={styles.singleInformation}>
+          <span>
+            <i className="fa-solid fa-check"></i>
+          </span>
+          <div>
+            I assure you that uploaded bank statements and provided company
+            information match and are of the same company, if there is a
+            mismatch then my report will not be generated
+          </div>
+        </div>
+      </div>
+      <div className={styles.information}>
+        <div className={styles.singleInformation}>
+          <span>
+            <i className="fa-solid fa-check"></i>
+          </span>
+          <div>
+            I understand that this is a general report based on the bank
+            statements and Credilinq is not providing a solution or guiding me
+            for my business growth
+          </div>
+        </div>
+      </div>
+      <div className={styles.information}>
+        <div className={styles.singleInformation}>
+          <span>
+            <i className="fa-solid fa-check"></i>
+          </span>
+          <div>I have read and understand the Terms & Conditions</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FormSectionFour;
