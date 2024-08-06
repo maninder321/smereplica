978826c1d3a541a4c10be061f1c6f04c5af2963b
maninder321 <maninderjitsingh380@gmail.com>
2024-08-07 00:02:14 +0530

    --added form section three structure

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -15,6 +15,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>SME Replica</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+          integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+          crossOrigin="anonymous"
+          referrerPolicy="no-referrer"
+        />
+      </head>
       <body className={inter.className} style={{ margin: 0, padding: 0 }}>
         {children}
       </body>
--- frontend/src/components/MainForm/children/FormSectionThird/FormSectionThird.tsx
+++ frontend/src/components/MainForm/children/FormSectionThird/FormSectionThird.tsx
@@ -1,7 +1,63 @@
 import React from "react";
+import styles from "./formSectionThird.module.css";
 
 function FormSectionThird() {
-  return <div>FormSectionThird</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.fileUploadBox}>
+        <div className={styles.fileUploadButton}>
+          <i className="fa-solid fa-file-arrow-up"></i>
+        </div>
+        <div className={styles.fileUploadText}>
+          <span style={{ textDecoration: "underline" }}>Click to Upload</span>{" "}
+          or Drag and Drop Bank Statements
+        </div>
+      </div>
+      <div className={styles.information}>
+        <div className={styles.singleInformation}>
+          <span>
+            <i className="fa-solid fa-check"></i>
+          </span>
+          <div>
+            PDFs (not scanned copies) of company's operating bank current
+            account(s) statements for the past 6 months. Example: If today is 06
+            Aug 24, then please upload bank statements from Feb 24 to Jul 24
+            (both months inclusive)
+          </div>
+        </div>
+
+        <div className={styles.singleInformation}>
+          <span>
+            <i className="fa-solid fa-check"></i>
+          </span>
+          <div>
+            If your company is multi-banked, then please upload 6 months bank
+            statements for each bank account
+          </div>
+        </div>
+
+        <div className={styles.singleInformation}>
+          <span>
+            <i className="fa-solid fa-check"></i>
+          </span>
+          <div>
+            If your file is password protected, we request you to remove the
+            password and upload the file to avoid submission failure
+          </div>
+        </div>
+
+        <div className={styles.singleInformation}>
+          <span>
+            <i className="fa-solid fa-check"></i>
+          </span>
+          <div>
+            In case if you are facing any issue while uploading bank statements,
+            Please contact us on support@credilinq.ai
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FormSectionThird;
